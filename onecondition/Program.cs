7a72010377a985f1bf18abc28e953bd6eb1813e8
s2018 <s2018s35@e-mirim.hs.kr>
2020-05-06 14:36:34 +0900

    초기 커밋.

---
+++ onecondition/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace onecondition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("숫자를 입력하세요");
+            x = int.Parse(Console.ReadLine());
+            if(x>10&& x < 20)
+            {
+                Console.WriteLine("조건에 맞습니다");
+            }
+        }
+    }
+}
